246bb30f372f06be816434aed04ecb3b73a23788
BASILAHAMED <sbasil.ahamed@gmail.com>
2023-09-02 16:14:08 +0530

    initial commit

---
+++ src/test/java/com/qa/nc/tests/LoginTest.java
@@ -0,0 +1,30 @@
+package com.qa.nc.tests;
+
+import org.testng.annotations.Test;
+
+import com.qa.nc.keyword.engine.KeyWordEngine;
+/**
+ * 
+ * @author BasilHameed
+ *
+ */
+public class LoginTest {
+	
+	public KeyWordEngine keyWordEngine;
+	
+	@Test
+	public void loginTest(){
+		keyWordEngine = new KeyWordEngine();
+		keyWordEngine.startExecution("login");
+	}
+	
+	@Test
+	public void signUpTest(){
+		keyWordEngine = new KeyWordEngine();
+		keyWordEngine.startExecution("signup");
+	}
+	
+	
+	
+
+}
